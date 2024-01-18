991727214ca08a3080e35d90d21956e4fd2cc449
Junior Dario Rotela Fretes <juniorrotela@MacBook-Pro-de-Junior.local>
2024-01-18 10:29:44 -0300

    ok

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,9 +1,12 @@
 import { Router } from "express";
-import { createUser, getUsers, updateUser } from "../controllers/user.controllers";
+import { createUser, deleteUser, getOneUser, getUsers, updateUser } from "../controllers/user.controllers";
 
 const router = Router()
 
 router.post("/users", createUser);
 router.get("/users", getUsers);
+router.get("/users/:id", getOneUser)
 router.put("/users/:id", updateUser)
+router.delete("/users/:id", deleteUser)
+
 export default router;
